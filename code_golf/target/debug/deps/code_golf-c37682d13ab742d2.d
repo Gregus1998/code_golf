9f5b6732df611d24eee8ea3acc75cefa8f342f4f
Bobby Sleipfisk <gregus.sorensen@gmail.com>
2024-06-06 06:54:31 +0000

    Adding cargo

---
+++ code_golf/target/debug/deps/code_golf-c37682d13ab742d2.d
@@ -0,0 +1,5 @@
+/home/cyfjonass/rust/code_golf/code_golf/target/debug/deps/libcode_golf-c37682d13ab742d2.rmeta: src/main.rs
+
+/home/cyfjonass/rust/code_golf/code_golf/target/debug/deps/code_golf-c37682d13ab742d2.d: src/main.rs
+
+src/main.rs:
---
+++ code_golf/target/debug/deps/code_golf-f15cf649c118807b.d
@@ -0,0 +1,5 @@
+/home/cyfjonass/rust/code_golf/code_golf/target/debug/deps/libcode_golf-f15cf649c118807b.rmeta: src/main.rs
+
+/home/cyfjonass/rust/code_golf/code_golf/target/debug/deps/code_golf-f15cf649c118807b.d: src/main.rs
+
+src/main.rs:
